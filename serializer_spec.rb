7e0b6eae3f6c284b6a4d0e29bfecdbe38b454743
Soumya Ray <soumya.ray@gmail.com>
2016-09-25 13:24:12 +0800

    Fix naming of specs

--- serializer_spec.rb
+++ serializer_spec.rb
@@ -4,13 +4,13 @@ require 'yaml'
 TEST_RECORDS = 22
 
 describe 'Serializer specs' do
-  it 'should convert yml to tsv' do
+  it 'should convert tsv to yml' do
     `ruby tsv_to_yml.rb programmers_survey_2014.tsv programmers_survey_2014.yml2`
     yml_output = YAML.load(File.read('programmers_survey_2014.yml2'))
     yml_output.count.must_equal TEST_RECORDS
   end
 
-  it 'should convert tsv to yml' do
+  it 'should convert yml to tsv' do
     `ruby yml_to_tsv.rb programmers_survey_2014.yml programmers_survey_2014.tsv2`
     tsv_output = File.read('programmers_survey_2014.tsv2').split("\n")
     tsv_output.count.must_equal(TEST_RECORDS + 1)
