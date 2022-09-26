4db3a2ae07d6c6217be1a50f8ce68ba4f41603c0
Soumya Ray <soumya.ray@gmail.com>
2022-09-26 21:56:22 +0800

    Add 2022 survey files

--- serializer_spec.rb
+++ serializer_spec.rb
@@ -3,10 +3,10 @@
 require 'minitest/autorun'
 require 'yaml'
 
-TSV_INFILE  = 'surveys/programmers_survey_2021.tsv'
-YML_INFILE  = 'surveys/programmers_survey_2021.yml'
-TSV_OUTFILE = 'programmers_survey_2021.tsv2'
-YML_OUTFILE = 'programmers_survey_2021.yml2'
+TSV_INFILE  = 'surveys/programmers_survey_2022.tsv'
+YML_INFILE  = 'surveys/programmers_survey_2022.yml'
+TSV_OUTFILE = 'programmers_survey_2022.tsv2'
+YML_OUTFILE = 'programmers_survey_2022.yml2'
 
 TSV_DATA = File.read(TSV_INFILE).split("\n")
 YML_DATA = YAML.safe_load(File.read(YML_INFILE))
